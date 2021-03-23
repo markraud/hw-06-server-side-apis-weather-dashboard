01a8c293cc6c19e4a6fc3b99abfed66c33d854c8
Mark Raudenbush <markraud@hotmail.com>
2021-03-22 20:17:48 -0600

    event listener working and fetching data from the API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,73 @@
 
-var searchCityBtn = document.querySelector('#search-btn');
-var currentCity = document.querySelector('#inlineFormInput').value;
-console.log(currentCity);
+$(document).ready(function () {
+    console.log("ready!");
 
-function searchCity(city) {
-    console.log(city);
-    var openWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q="+ city +"&APPID=f1d7def2b5aef9f0636bcc751ef47a58";
-    console.log(openWeatherURL);
+    var searchCityBtn = document.querySelector('#search-btn');
 
-    fetch(openWeatherURL)
-    .then(function(res) {
-        return res.json();
-    })
-    .then(function(data) {
-        console.log(data);
-      $("#currentCity").html("<div>this is a test</div");
+    // function handleSearchFormSubmit(event) {
+    //     event.preventDefault();
+
+    //     var currentCity = document.querySelector('#inlineFormInput').value;
+    //     console.log(currentCity);
+
+    //     if (!currentCity) {
+    //       console.error('You need a search input value!');
+    //       return;
+    //     }
+
+    //     searchCity(currentCity);
+    //   }
+
+
+    searchCityBtn.addEventListener('click', function (event) {
+        event.preventDefault();
+        var temp;
+        var humidity;
+        var windSpeed;
+        var uvIndex;
+        var lat;
+        var lon;
+        var uvi;
+
+        var currentCity = document.querySelector('#inlineFormInput').value;
+        console.log(currentCity);
+        var openWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&units=imperial&APPID=f1d7def2b5aef9f0636bcc751ef47a58";
+        console.log(openWeatherURL);
+        var uvURL;
+
+        fetch(openWeatherURL)
+            .then(function (res) {
+                return res.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                temp = data.main.temp;
+                humidity = data.main.humidity;
+                windSpeed = data.wind.speed;
+                lat = data.coord.lat;
+                lon = data.coord.lon;
+                uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=f1d7def2b5aef9f0636bcc751ef47a58";
+                console.log(temp);
+                console.log(humidity);
+                console.log(windSpeed);
+                console.log(lon);
+                console.log(lat);
+                // $("#currentCity").html("<div>this is a test</div");
+
+            })
+            .then(function () {
+                fetch(uvURL)
+                    .then(function (res) {
+                        return res.json();
+                    })
+                    .then(function (data) {
+                        uvi = data.value;
+
+                    })
+                console.log(currentCity);
+                $("#current-city").html("<h3>" + currentCity + "</h3>");  //this isn't working for some reason
+                $("#current-city").html("<div>This is the temperature:" + temp + "</div");
+            });
     });
-}
 
-// $("#search-city").click(function(){
-//     searchCity();
-// })
-
-
-
-// function handleSearchFormSubmit(event) {
-//     event.preventDefault();
-  
-//     var currentCity = document.querySelector('#inlineFormInput').value;
-//     console.log(currentCity);
-    
-//     if (!currentCity) {
-//       console.error('You need a search input value!');
-//       return;
-//     }
-  
-//     searchCity(currentCity);
-//   }
-  
-
-searchCityBtn.addEventListener('submit',searchCity(currentCity));
+});
