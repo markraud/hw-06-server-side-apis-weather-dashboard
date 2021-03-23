138dd121fa30a9497194f334bec377395504c768
Mark Raudenbush <markraud@hotmail.com>
2021-03-22 18:49:39 -0600

    work on event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 
-function searchApi() {
-    var openWeatherURL = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=f1d7def2b5aef9f0636bcc751ef47a58';
+var searchCityBtn = document.querySelector('#search-btn');
+var currentCity = document.querySelector('#inlineFormInput').value;
+console.log(currentCity);
+
+function searchCity(city) {
+    console.log(city);
+    var openWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q="+ city +"&APPID=f1d7def2b5aef9f0636bcc751ef47a58";
+    console.log(openWeatherURL);
 
     fetch(openWeatherURL)
     .then(function(res) {
@@ -8,8 +14,29 @@ function searchApi() {
     })
     .then(function(data) {
         console.log(data);
-      $("#currentCity").html("<div>"+data[0]+"</div");
+      $("#currentCity").html("<div>this is a test</div");
     });
 }
 
-searchApi()
+// $("#search-city").click(function(){
+//     searchCity();
+// })
+
+
+
+// function handleSearchFormSubmit(event) {
+//     event.preventDefault();
+  
+//     var currentCity = document.querySelector('#inlineFormInput').value;
+//     console.log(currentCity);
+    
+//     if (!currentCity) {
+//       console.error('You need a search input value!');
+//       return;
+//     }
+  
+//     searchCity(currentCity);
+//   }
+  
+
+searchCityBtn.addEventListener('submit',searchCity(currentCity));
