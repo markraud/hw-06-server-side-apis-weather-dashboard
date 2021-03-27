3107ade1f853769d7f829f6e298855e1fdb8e984
Mark Raudenbush <markraud@hotmail.com>
2021-03-26 22:25:09 -0600

    cleaned up the current weather section to display in the right order

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
                 lat = data.coord.lat;
                 lon = data.coord.lon;
                 icon= data.weather[0].icon;
-                uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=f1d7def2b5aef9f0636bcc751ef47a58";
+                uvUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=f1d7def2b5aef9f0636bcc751ef47a58";
                 console.log(temp);
                 console.log(humidity);
                 console.log(windSpeed);
@@ -44,33 +44,29 @@ $(document).ready(function () {
                 console.log(lat);
                 console.log(city);
                 console.log(icon);
-                // $("#currentCity").html("<div>this is a test</div");
+                $("#intro").hide();
+                //$("#intro").addClass("d-none")
+                $("#current-city").append("<div class='h2 p-2 ml-2 d-inline-block font-weight-bold mr-2'>" + city + "</div>");
+                var currentDate = moment();
+                $("#current-city").append("(" + currentDate.format('L') +")");
+                $("#current-city").append("<div class='p-2 m-2'>Temperature: " + temp + " ℉</div>");
+                $("#current-city").append("<div class='p-2 m-2'>Humidity: " + humidity + "%</div>");
+                $("#current-city").append("<div class='p-2 m-2'>Wind Speed: " + windSpeed + " MPH</div>");
 
             })
             .then(function () {
-                fetch(uvURL)
+                fetch(uvUrl)
                     .then(function (res) {
                         return res.json();
                     })
                     .then(function (data) {
-                        console.log(data);
                         uvi = data.value;
-                        console.log(uvi);
-
+                        //$("#uv-index").text("UV Index: " + uvi);
+                        $("#current-city").append("<div class='p-2 m-2'>UV Index: " + uvi + "</div>");
                     })
-                // console.log(uvi);
-                $("#intro").hide();
-                //$("#intro").addClass("d-none")
-                $("#current-city").children().first().text(city);
-                var currentDate = moment();
-                $("#current-city").append("(" + currentDate.format('L') +")");
-                $("#current-city").append("<div class='y-1'>Temperature: " + temp + " ℉</div>");
-                $("#current-city").append("<div>Humidity: " + humidity + "</div>");
-                $("#current-city").append("<div>UV Index: " + uvi + "</div>");
+            })
+        // another fetch to get the 5 day forcast
+        //var forcastUrl = 
 
-
-
-            });
     });
-
 });
