2be7fffa7b68f8dc00915d028962b1a5832d8a6b
Mark Raudenbush <markraud@hotmail.com>
2021-03-28 01:18:31 -0600

    saved previous cities to local and displayed in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,23 @@ $(document).ready(function () {
 
     var searchCityBtn = document.querySelector('#search-btn');
 
-    
-    searchCityBtn.addEventListener('click', function (event) {
+    // searchCityBtn.addEventListener('click', function (event) {
+    function getWeather() {
         event.preventDefault();
+        //empty previous search results
+        $("#current-city").empty();
+        $("#forecastInfo").empty();
+
         var temp ='';
         var humidity ='';
         var windSpeed ='';
-        var uvIndex ='';
+        //var uvIndex ='';
         var lat ='';
         var lon ='';
         var uvi ='';
         var city ='';
         var icon ='';
-
+        var localStorCity = '';
         var currentCity = document.querySelector('#inlineFormInput').value;
         var openWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&units=imperial&APPID=f1d7def2b5aef9f0636bcc751ef47a58";
         //console.log(openWeatherURL);
@@ -31,7 +35,6 @@ $(document).ready(function () {
                 return res.json();
             })
             .then(function (data) {
-                //console.log(data);
                 city = data.name;
                 temp = data.main.temp;
                 humidity = data.main.humidity;
@@ -42,22 +45,18 @@ $(document).ready(function () {
                 uvUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=f1d7def2b5aef9f0636bcc751ef47a58";
                 forecastUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=f1d7def2b5aef9f0636bcc751ef47a58";
                 iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
-                // console.log(temp);
-                // console.log(humidity);
-                // console.log(windSpeed);
-                // console.log(lon);
-                // console.log(lat);
-                // console.log(city);
+                localStorage.setItem("city",city);
                 $("#intro").hide();
+                $("#main").addClass("border border-secondary")
                 $("#current-city").append("<div class='h2 p-2 ml-2 d-inline-block font-weight-bold mr-2'>" + city + "</div>");
-                //var currentDate = moment();
                 $("#current-city").append("(" + currentDate.format('L') +")");
                 tag = '<img src="' + iconUrl + '" alt="weather icon">'
                 $("#current-city").append(tag);
-
                 $("#current-city").append("<div class='p-2 m-2'>Temperature: " + temp + " ℉</div>");
                 $("#current-city").append("<div class='p-2 m-2'>Humidity: " + humidity + "%</div>");
                 $("#current-city").append("<div class='p-2 m-2'>Wind Speed: " + windSpeed + " MPH</div>");
+                localStorCity = localStorage.getItem("city");
+                $("#prev-cities").append('<button onclick="searchPrev();" type="button" class="list-group-item list-group-item-action">' + localStorCity + '</button>')
 
             })
             .then(function () {
@@ -71,16 +70,12 @@ $(document).ready(function () {
                     })
             })
             .then(function () { // another fetch to get the 5 day forcast
-                //console.log(forecastUrl);
                 fetch(forecastUrl)
                     .then(function (res){
                         return res.json();
                     })
                     .then(function (data) {
-                        // var x = data.list[2].dt_txt
-                        // var x2 = x.split(' ');
-                        // console.log('recieved data', x2);
-                        forcast="";
+                        var forcast="";
                         $.each(data.list, function(index, val) {
                             // grab forcast objects in data.list for hour = 12 so we get 1 forcast per day
                             if (((val.dt_txt.split(' '))[1]) === '12:00:00' ) {
@@ -89,7 +84,7 @@ $(document).ready(function () {
                                 forecastDate = (forecastDate.split("-"));
                                 gregDate = forecastDate[1] + "-" + forecastDate[2] + "-" + forecastDate[0];
                                 console.log(gregDate);
-                                // inject forcast to html
+                                // build forcast variable with html
                                 forcast += "<div class='card bg-primary text-white p-2 m-2' style='width: 10rem;>" // Open div for card tag
                                 forcast += "<div class='card-body'>"
                                 forcast += "<h6 class='card-title'>" + gregDate + "</h6>" 
@@ -99,27 +94,28 @@ $(document).ready(function () {
                                 forcast += "</div>"    // close card body
                                 forcast += "</div>" // Close div for card tag
                                 $('#forcastTitle').removeClass('d-none');
+                                //inject forcast into HTML
                                 $("#forecastInfo").html(forcast);
                             }
-                          });
-                        //console.log(data.list);
-                        //console.log(forcast);
+                        });
                         
-                        // $("#forecastTitle").append(`<div class='card bg-primary text-white p-2 m-2' style='width: 10rem;'>
-                        //                                 <div class='card-body'>
-                        //                                     <h6 id='day-2' class='card-title'></h6>
-                        //                                 </div>
-                        //                             </div>`);
-                        // $("#day-2").append(day2);
-                        // $("#day-2").append();   // append icon
-                        // $("#day-2").append();   // append temp
-                        // $("#day-2").append();   // append humidity
-
-
-
-
                     })
-            })
-        
-    });
+                })
+            $("#inlineFormInput").val("");  // clear out the search input
+            
+    }//);
+
+    searchCityBtn.addEventListener("click", function() {
+        getWeather();
+      });
+
+    // TO DO:  
+    // create a function to grab previous cities from local storage
+    // make UV index display background color
+    
+
+
+
+
+            
 });
